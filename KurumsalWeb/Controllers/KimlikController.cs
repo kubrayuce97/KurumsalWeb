9ab69b928c20034fae7eae568d5c2420ad7c3e71
kubrayuce97 <kubrayuce97@gmail.com>
2021-08-13 10:48:21 +0300

    Proje dosyası ekle.

---
+++ KurumsalWeb/Controllers/KimlikController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class KimlikController : Controller
+    {
+        // GET: Kimlik
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Kimlik/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Kimlik/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kimlik/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Kimlik/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Kimlik/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Kimlik/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Kimlik/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ KurumsalWeb/Models/Model/Kimlik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace KurumsalWeb.Models.Model
+{
+    [Table("Kimlik")]
+    public class Kimlik
+    {
+        [Key]
+        public int KimlikId { get; set; }
+        [DisplayName("Site Başlık")]
+        [Required,StringLength(100,ErrorMessage ="100 karakter olmalıdır.")]
+        public string Title { get; set; }
+        [DisplayName("Anahtar Kelimeler")]
+        [Required, StringLength(200, ErrorMessage = "200 karakter olmalıdır.")]
+        public string Keywords { get; set; }
+        [DisplayName("Site Açıklama")]
+        [Required, StringLength(100, ErrorMessage = "300 karakter olmalıdır.")]
+        public string Description { get; set; }
+        [DisplayName("Site Logo")]
+        public string logoURL { get; set; }
+        [DisplayName("Site Ünvan")]
+        public string Unvan { get; set; }
+    }
+}
