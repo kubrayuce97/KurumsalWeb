9934e1c901c424519fef097502ef6aa625aa22d9
kubrayuce97 <kubrayuce97@gmail.com>
2021-08-20 09:48:53 +0300

    Git deneme1

--- KurumsalWeb/Controllers/AdminController.cs
+++ KurumsalWeb/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
 ﻿using KurumsalWeb.Models;
+using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +11,46 @@ namespace KurumsalWeb.Controllers
 {
     public class AdminController : Controller
     {
+        KurumsalDBContext db = new KurumsalDBContext();
         // GET: Admin
-       
+
         public ActionResult Index()
         {
-          
+            var sorgu = db.Kategori.ToList();
+            return View(sorgu);
+        }
+        public ActionResult Login()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var login = db.Admin.Where(x => x.Eposta == admin.Eposta).SingleOrDefault();
+
+            if (login != null && login.Eposta == admin.Eposta && login.Sifre == admin.Sifre)
+            {
+                Session["adminid"] = login.AdminId;
+                Session["eposta"] = login.Eposta;
+
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                ViewBag.Uyari = "Kullanıcı adı ya da şifre yanlış";
+            }
+
+            return View(admin);
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session["adminid"] = null;
+            Session["eposta"] = null;
+            Session.Abandon();
+
+            return RedirectToAction("Login", "Admin");
         }
     }
 }
