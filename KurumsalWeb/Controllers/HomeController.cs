824a83798d9d0e873b1e5da48b6d17dc9a26f1e5
kubrayuce97 <kubrayuce97@gmail.com>
2021-08-26 23:38:11 +0300

    İletişim sayfasındaki form doldurularak mail atıldı.

--- KurumsalWeb/Controllers/HomeController.cs
+++ KurumsalWeb/Controllers/HomeController.cs
@@ -4,11 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace KurumsalWeb.Controllers
 {
-    
+
     public class HomeController : Controller
     {
         private KurumsalDBContext db = new KurumsalDBContext();
@@ -29,7 +30,7 @@ namespace KurumsalWeb.Controllers
         }
         public ActionResult HizmetPartial()
         {
-            return View(db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId));
+            return View(db.Hizmet.ToList().OrderByDescending(x => x.HizmetId));
         }
         public ActionResult Hakkimizda()
         {
@@ -37,11 +38,30 @@ namespace KurumsalWeb.Controllers
         }
         public ActionResult Hizmetlerimiz()
         {
-            return View(db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId));
+            return View(db.Hizmet.ToList().OrderByDescending(x => x.HizmetId));
         }
         public ActionResult Iletisim()
         {
             return View(db.Iletisim.SingleOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Iletisim(string adsoyad = null, string email = null, string konu = null, string mesaj = null)
+        {
+            if (adsoyad != null && email != null)
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "kurumsalweb04@gmail.com";//gmail adres yazılacak.
+                WebMail.Password = "KurumsalWeb04";//gmailin şifresi girilecek.
+                WebMail.SmtpPort = 587;
+                WebMail.Send("kurumsalweb04@gmail.com", konu, email + "-" + mesaj);
+                ViewBag.Uyari = "Mesajınız başarı ile gönderildi.";
+            }
+            else
+            {
+                ViewBag.Uyari = "Hata oluştu tekrar deneyiniz";
+            }
+            return View();
         }
         public ActionResult FooterPartial()
         {
