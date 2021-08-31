979ab6307f2f088d55f50ce69331585835ffdffb
kubrayuce97 <kubrayuce97@gmail.com>
2021-08-31 15:54:47 +0300

    Admin paneli  kullanıcı bilgileri güncelleme ve Crypto helper ile şifreleme yapıldı.

--- KurumsalWeb/Controllers/AdminController.cs
+++ KurumsalWeb/Controllers/AdminController.cs
@@ -69,12 +69,32 @@ namespace KurumsalWeb.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create(Admin admin,string sifre,string eposta)
+        public ActionResult Create(Admin admin, string sifre, string eposta)
         {
             if (ModelState.IsValid)
             {
                 admin.Sifre = Crypto.Hash(sifre, "MD5");
                 db.Admin.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+            }
+            return View(admin);
+        }
+        public ActionResult Edit(int id)
+        {
+            var a = db.Admin.Where(x => x.AdminId == id).SingleOrDefault();
+            return View(a);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Admin admin, string sifre, string eposta)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var a = db.Admin.Where(x => x.AdminId == id).SingleOrDefault();
+                a.Sifre = Crypto.Hash(sifre, "MD5");
+                a.Eposta = admin.Eposta;
+                a.Yetki = admin.Yetki;
                 db.SaveChanges();
                 return RedirectToAction("Adminler");
             }
